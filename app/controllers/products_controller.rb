cc246e08f8769344b4d78598e792a2e851738624
ghenaog <ghenaog@gmail.com>
2016-10-22 10:31:38 -0500

    Avanzando en la tienda de memo

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,39 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = 'The product has been saved'
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
   def edit
+    @product = Product.find(params[:id])
   end
 
   def index
+    @products = Product.all
   end
 
   def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    flash[:notice] = 'Product was successfully destroyed'
+    redirect_to products_path
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  private
+
+    def product_params
+      params.require(:product).permit(:name, :reference, :price, :quantity, :brand, :description)
+    end
+
 end
