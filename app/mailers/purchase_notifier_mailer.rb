09a4bf201681e4f7e9c06501754010a626af9958
ghenaog <ghenaog@gmail.com>
2016-11-17 18:59:20 -0500

    Add mailer model

---
+++ app/mailers/purchase_notifier_mailer.rb
@@ -0,0 +1,11 @@
+class PurchaseNotifierMailer < ApplicationMailer
+
+  default :form => 'info@latiendadememo.com'
+
+  def notify_account_balance(user, balance)
+    @user = user
+    @balance = balance
+
+    mail(to: user.email, subject: 'Es hora de conversar con Memo..!')
+
+end
